b9765f5e5a978bc5c188986c0785995afab81045
ArberSh <arbershaska2@gmail.com>
2023-12-03 22:15:34 +0100

    Fixing some errors

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import Nav from './pages/Nav'
 import Home from './pages/Home'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Results from './pages/Results';
-import Api from './Api';
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Nav/>
-       <Api/>
       <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path='/search' element={<Results/>}/>
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,20 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Results.css';
+import axios from 'axios'
 
-function Results(data) {
-    console.log(data.message)
+function Results() {
+    const [posts,setPosts] = useState([]);
+   
+    useEffect(() => {
+        async function fetchFood() {
+            try{
+            const {data} = await axios.get(`https://www.themealdb.com/api/json/v1/1/random.php`)
+            setPosts(data)  
+            console.log(data)     
+        }
+        catch(error) {
+            console.log("Error" , error)
+        }
+        }
+        fetchFood()
+    },[]);
 
+    // console.log(posts)
     return (
         <div className='Container'>
-            <div className='Food_Container'>
-                <div className='Food_Image'>
-                    <img src='https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg' alt='Food'></img>
-                </div>
-                <div className='SmallDescription'>
-                    <h2>{data.message}</h2>
-                </div>
+        <div className='Food_Container'>
+            <div className='Food_Image'>
+                <img src='https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg' alt='Food'></img>
+            </div>
+            <div className='SmallDescription'>
+                <h2>{posts}
+                </h2>
             </div>
         </div>
+    </div>  
     );
 }
 
